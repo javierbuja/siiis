b6a1a89f99b487cdda8108959aa586ba57db58ce
Christian Bugiani <christianbugiani2005@gmail.com>
2025-01-14 17:37:19 +0100

    init repo

---
+++ Assets/Lesson-3/Scripts/PokeContainer.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable, Inspectable]
+public class PokeContainer
+{
+    [Inspectable]
+    public string Name;
+    [Inspectable]
+    public Sprite PkmImg;
+}
